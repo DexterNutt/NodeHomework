5263f100c116f317bae8dae513d17f358736d753
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-01-18 16:19:56 +0100

    Add even case to check if name.length is even/odd

--- C5_Servers/code.js
+++ C5_Servers/code.js
@@ -1,17 +1,24 @@
-const http = require("http");
+const http = require('http');
 
 const handler = (req, res) => {
-  console.log(req.url);
-  console.log(req.url.split("/"));
-  const [_, op, name] = req.url.split("/");
+  // console.log(req.url);
+  // console.log(req.url.split('/'));
+  const [_, op, name] = req.url.split('/');
   switch (op) {
-    case "length":
-      let o;
-      o = name.length;
-      res.end(`${name} is ${o} characters long`);
+    case 'length':
+      let length = name.length;
+      res.end(`${name} is ${length} characters long`);
       break;
-    case "vowels":
-      const vowels = "AaEeIiOoUu";
+
+    case 'even':
+      let even = name.length % 2;
+      if (even === 0) {
+        res.end(`${name} is even`);
+      } else res.end(`${name} is odd`);
+      break;
+
+    case 'vowels':
+      const vowels = 'AaEeIiOoUu';
       let vowelCount = 0;
       for (let i = 0; i < name.length; i++) {
         if (vowels.indexOf(name[i]) !== -1) {
@@ -20,8 +27,9 @@ const handler = (req, res) => {
       }
       res.end(`There are ${vowelCount} vowels in ${name}`);
       break;
-    case "consonants":
-      const consonants = "BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz";
+
+    case 'consonants':
+      const consonants = 'BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz';
       let consonantCount = 0;
       for (let i = 0; i < name.length; i++) {
         if (consonants.indexOf(name[i]) !== -1) {
@@ -30,14 +38,19 @@ const handler = (req, res) => {
       }
       res.end(`There are ${consonantCount} consonants in ${name}`);
       break;
+
     default:
-      res.end("There is no such operation");
+      res.end('There is no such operation');
   }
 };
 
 const server = http.createServer(handler);
 
-server.listen(10000, "127.0.0.1", (err) => {
-  if (err) console.log("error");
-  console.log("server started on port 10000");
+server.listen(10000, '127.0.0.1', (err) => {
+  if (err) console.log('error');
+  console.log('server started on port 10000');
 });
+
+//* 1
+//* 2
+//* 3
