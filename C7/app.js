e9d68a4c352e9dd9cd386b12a6d5cd6bc6e52f84
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-01-29 16:00:54 +0100

    Add button to add new tasks

--- C7/app.js
+++ C7/app.js
@@ -1,1 +1,24 @@
 //! TO DO APP
+const express = require('express');
+const app = express();
+
+// CODE
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+let tasks = [];
+
+app.get('/', (req, res) => {
+  res.render('index', { tasks });
+});
+
+app.post('/add', (req, res) => {
+  const task = req.body.task;
+  tasks.push(task);
+  res.redirect('/'); //! VITAL
+});
+
+app.listen(10000, (err) => {
+  if (err) console.log(err);
+  console.log(`Server initiated on port 10000`);
+});
